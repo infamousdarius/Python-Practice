eb24712f004f5f932bba350fb874a24932f3aa2f
misranrifat <mrifat@tistatech.com>
2019-07-17 12:45:07 -0400

    adding new classes

--- Fun Things/Browsers/OPen a browser.py
+++ Fun Things/Browsers/OPen a browser.py
@@ -3,4 +3,8 @@ import webbrowser
 url = 'http://docs.python.org/'
 # webbrowser.open(url)
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-webbrowser.get(chrome_path).open(url)
+
+# i = 1
+# while i <= 10:
+#     webbrowser.get(chrome_path).open(url)
+#     i += 1
