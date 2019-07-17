21a22a506048d1902016703ccb17d28950696869
misranrifat <mrifat@tistatech.com>
2019-07-17 10:55:02 -0400

    adding new classes

--- Core_Python/Python Classes and Objects/The __init__() Function.py
+++ Core_Python/Python Classes and Objects/The __init__() Function.py
@@ -1,3 +1,15 @@
+# The examples above are classes and objects in their simplest form, and are not really useful in real life
+# applications.
+#
+# To understand the meaning of classes we have to understand the built-in __init__() function.
+#
+# All classes have a function called __init__(), which is always executed when the class is being initiated.
+#
+# Use the __init__() function to assign values to object properties, or other operations that are necessary to do
+# when the object is being created:
+
+
+# Create a class named Person, use the __init__() function to assign values for name and age:
 class Person:
     def __init__(self, name, age):
         self.name = name
